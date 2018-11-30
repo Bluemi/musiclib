09741886e8c15663ff690762462244ea6c2f761f
Bluemi <bruno.schilling@protonmail.ch>
2018-11-30 01:06:16 +0100

    reworked rhythm

--- src/rhythm.rs
+++ src/rhythm.rs
@@ -1,11 +1,11 @@
-use std::ops::Add;
+use std::ops::{Add,Sub,AddAssign};
 use num_rational::Rational32;
-use num_traits::identities::Zero;
 use std::fmt;
+use std::convert::From;
 
-#[derive(Clone, Copy, PartialEq)]
+#[derive(Clone, Copy, PartialEq, PartialOrd)]
 pub struct Duration {
-	pub duration: Rational32,
+	duration: Rational32,
 }
 
 impl Duration {
@@ -28,14 +28,36 @@ impl Duration {
 	pub fn sixteenth() -> Duration {
 		Duration { duration: Rational32::new(1, 16) }
 	}
+
+	pub fn from_rational(rational: Rational32) -> Duration {
+		Duration { duration: rational }
+	}
+}
+
+impl From<Duration> for Rational32 {
+	fn from(duration: Duration) -> Rational32 {
+		duration.duration
+	}
+}
+
+impl From<BarTimePoint> for Duration {
+	fn from(time_point: BarTimePoint) -> Duration {
+		Duration { duration: time_point.time_point }
+	}
 }
 
 /*
  * A 4/4 Bar has the BarTimePoints: [0/4, 1/4, 2/4, 3/4]
  */
-#[derive(Clone, Copy, PartialEq)]
+#[derive(Clone, Copy, PartialEq, PartialOrd)]
 pub struct BarTimePoint {
-	pub time_point: Rational32,
+	time_point: Rational32,
+}
+
+impl BarTimePoint {
+	pub fn new(nominator: u32, denominator: u32) -> BarTimePoint {
+		BarTimePoint { time_point: Rational32::new(nominator as i32, denominator as i32) }
+	}
 }
 
 impl Add<Duration> for BarTimePoint {
@@ -46,10 +68,42 @@ impl Add<Duration> for BarTimePoint {
 	}
 }
 
+impl AddAssign<Duration> for BarTimePoint {
+	fn add_assign(&mut self, duration: Duration) {
+		self.time_point += duration.duration;
+	}
+}
+
+impl Sub<Duration> for BarTimePoint {
+	type Output = BarTimePoint;
+
+	fn sub(self, duration: Duration) -> BarTimePoint {
+		BarTimePoint { time_point: self.time_point - duration.duration }
+	}
+}
+
+impl From<BarTimeSignature> for BarTimePoint {
+	fn from(bar_time_signature: BarTimeSignature) -> BarTimePoint {
+		BarTimePoint { time_point: Rational32::new(bar_time_signature.nominator as i32, bar_time_signature.denominator as i32) }
+	}
+}
+
+impl From<BarTimePoint> for Rational32 {
+	fn from(time_point: BarTimePoint) -> Rational32 {
+		time_point.time_point
+	}
+}
+
+impl From<Rational32> for BarTimePoint {
+	fn from(rational: Rational32) -> BarTimePoint {
+		 BarTimePoint { time_point: rational }
+	}
+}
+
 #[derive(Clone, Copy, PartialEq)]
 pub struct BarTimeSignature {
-	pub nominator: u32,
-	pub denominator: u32,
+	nominator: u32,
+	denominator: u32,
 }
 
 impl BarTimeSignature {
@@ -69,15 +123,32 @@ impl BarTimeSignature {
 		BarTimeSignature { nominator: 6, denominator: 8 }
 	}
 
-	pub fn get_ratio(self) -> Rational32 {
-		Rational32::new(self.nominator as i32, self.denominator as i32)
+	pub fn get_nominator(self) -> u32 {
+		self.nominator
+	}
+
+	pub fn get_denominator(self) -> u32 {
+		self.denominator
+	}
+
+	pub fn new(nominator: u32, denominator: u32) -> BarTimeSignature {
+		BarTimeSignature { nominator, denominator }
 	}
 }
 
 #[derive(Clone, Copy, PartialEq)]
 pub struct RhythmNote {
-	pub time_point: BarTimePoint,
-	pub duration: Duration,
+	time_point: BarTimePoint,
+	duration: Duration,
+}
+
+impl RhythmNote {
+	pub fn new(time_point: BarTimePoint, duration: Duration) -> RhythmNote {
+		RhythmNote {
+			time_point,
+			duration
+		}
+	}
 }
 
 impl fmt::Debug for RhythmNote {
@@ -90,26 +161,19 @@ impl fmt::Debug for RhythmNote {
 	}
 }
 
-impl RhythmNote {
-	pub fn new(time_point: Rational32, duration: Rational32) -> RhythmNote {
-		RhythmNote {
-			time_point: BarTimePoint { time_point },
-			duration: Duration { duration }
-		}
-	}
-}
-
 pub struct RhythmPattern {
 	pub notes: Vec<RhythmNote>,
 }
 
 impl RhythmPattern {
-	pub fn straight_rhythm_notes(bar_time_signature: BarTimeSignature, duration: Rational32) -> RhythmPattern {
-		let mut counter = Rational32::zero();
+	pub fn straight_rhythm_notes(bar_time_signature: BarTimeSignature, duration: Duration) -> RhythmPattern {
+		let mut counter = BarTimePoint::new(0, 1);
 
 		let mut notes: Vec<RhythmNote> = vec![RhythmNote::new(counter, duration)];
 
-		while counter < (bar_time_signature.get_ratio() - duration) {
+		let end: BarTimePoint = BarTimePoint::from(bar_time_signature) - duration;
+
+		while counter < end {
 			counter += duration;
 			notes.push(RhythmNote::new(counter, duration))
 		}
@@ -124,13 +188,13 @@ mod tests {
 
 	#[test]
 	pub fn test_straight_rhythm_notes() {
-		let rhythm_pattern: RhythmPattern = RhythmPattern::straight_rhythm_notes(BarTimeSignature::four_quarter_time(), Duration::quarter().duration);
-		let asserted_pattern: Vec<RhythmNote> = vec![
-			RhythmNote::new(Rational32::new(0, 4), Rational32::new(1, 4)),
-			RhythmNote::new(Rational32::new(1, 4), Rational32::new(1, 4)),
-			RhythmNote::new(Rational32::new(2, 4), Rational32::new(1, 4)),
-			RhythmNote::new(Rational32::new(3, 4), Rational32::new(1, 4)),
+		let rhythm_pattern1: RhythmPattern = RhythmPattern::straight_rhythm_notes(BarTimeSignature::four_quarter_time(), Duration::quarter());
+		let asserted_pattern1: Vec<RhythmNote> = vec![
+			RhythmNote::new(BarTimePoint::new(0, 4), Duration::quarter()),
+			RhythmNote::new(BarTimePoint::new(1, 4), Duration::quarter()),
+			RhythmNote::new(BarTimePoint::new(2, 4), Duration::quarter()),
+			RhythmNote::new(BarTimePoint::new(3, 4), Duration::quarter()),
 		];
-		assert_eq!(rhythm_pattern.notes, asserted_pattern);
+		assert_eq!(rhythm_pattern1.notes, asserted_pattern1);
 	}
 }
